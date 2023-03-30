3b3f3b6d34f9588bc69214e946ce8d16a5f143a9
ash1407 <68329823+ash1407@users.noreply.github.com>
2023-03-31 00:45:09 +0530

    Update Streamlit_stock_prediction.py

--- stock/Streamlit_stock_prediction.py
+++ stock/Streamlit_stock_prediction.py
@@ -52,7 +52,8 @@ with col2:
 if st.button('Predicted Stock Price'):
     stock_prediction = Stock_price_model.predict(news)
     stock_price = current_price + stock_prediction
-    st.sucess('Stock_Price :', stock_price)
+    #st.sucess('Stock_Price :', stock_price)
+    st.write('Stock_Price :', stock_price)
 
 
 
