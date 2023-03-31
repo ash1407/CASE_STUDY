213a4787967c71063f93ff9c5a6699534cd753b3
ash1407 <68329823+ash1407@users.noreply.github.com>
2023-04-01 01:00:12 +0530

    Update Streamlit_stock_prediction.py

--- stock/Streamlit_stock_prediction.py
+++ stock/Streamlit_stock_prediction.py
@@ -1,55 +1,27 @@
-
-import streamlit as st
-import pandas as pd
-import numpy as np
 import pickle
 import json
-from sklearn.feature_extraction.text import TfidfVectorizer
+import streamlit as st
+import numpy as np
 
 # loading the saved models
-xgb_model = json.loads(open('/app/case_study/stock/model_tfidf.json', 'r').read())
+Stock_price_model = json.loads(open('/app/case_study/stock/model.json', 'r').read())
 
+# Load the pre-trained tf-idf vectorizer
+with open('/app/case_study/stock/tfidf.pickle', 'rb') as f:
+    tfidf = pickle.load(f)
+    
 # Define a function to preprocess the text input
-def preprocess_text(text):
-    text = text.lower()
-    text = text.replace(',', '')
-    text = text.replace('.', '')
-    text = text.replace('!', '')
-    text = text.replace('?', '')
-    text = text.replace('(', '')
-    text = text.replace(')', '')
-    text = text.replace('[', '')
-    text = text.replace(']', '')
-    text = text.replace('{', '')
-    text = text.replace('}', '')
-    text = text.replace(':', '')
-    text = text.replace(';', '')
-    text = text.replace('-', '')
-    text = text.replace('_', '')
-    text = text.replace('/', ' ')
-    text = text.replace('\\', ' ')
-    text = text.replace('@', '')
-    text = text.replace('#', '')
-    text = text.replace('$', '')
-    text = text.replace('%', '')
-    text = text.replace('^', '')
-    text = text.replace('&', '')
-    text = text.replace('*', '')
-    text = text.replace('~', '')
-    text = text.replace('`', '')
-    text = text.replace('=', '')
-    text = text.replace('+', '')
-    text = text.replace('|', '')
-    text = text.strip()
-    return text
+def preprocess_text():
+    news = st.text_input('Current News Related to Stock')
+    return news
 
-# Define the Streamlit app
+# Define the Streamlit apps
 def app():
     # page title
     st.title('Stock Price Prediction using ML')
 
     # Text input
-    text_input = st.text_input('Current News Related to Stock')
+    text_input = preprocess_text()
 
     # Numeric input
     current_price = st.number_input('Current Price')
@@ -57,22 +29,19 @@ def app():
     # When the user clicks the 'Predict' button, preprocess the input and pass it to the model
     if st.button('Predict'):
         # Preprocess the text input
-        preprocessed_input = preprocess_text(text_input)
-
-        # Convert the preprocessed input to a numpy array
-        input_array = np.array([preprocessed_input])
-
+        preprocessed_input = tfidf.transform([preprocess_text()])
+        
         # Use the pre-trained model to make a prediction
-        stock_prediction = xgb_model.predict(xgb.DMatrix(input_array))
+        stock_prediction = xgb_model.predict(preprocessed_input)
 
         # Predict final stock value
-        stock_price = float(current_price + stock_prediction)
+        stock_price = current_price + stock_prediction[0]
+        
 
         # Display the prediction to the user
         st.write('Prediction:', stock_price)
-        
-        
+
 # Run the Streamlit app
 if __name__ == '__main__':
     app()
-        
+	
