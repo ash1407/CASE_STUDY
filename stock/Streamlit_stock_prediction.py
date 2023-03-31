7ded2b1856d89f0ef747e84f7ec567e20c7bbd74
ash1407 <68329823+ash1407@users.noreply.github.com>
2023-03-31 23:50:29 +0530

    Update Streamlit_stock_prediction.py

--- stock/Streamlit_stock_prediction.py
+++ stock/Streamlit_stock_prediction.py
@@ -1,59 +1,48 @@
 import pickle
 import json
 import streamlit as st
-from streamlit_option_menu import option_menu
-
+import numpy as np
 
 # loading the saved models
-
-#Stock_price_model = pickle.load(open('/app/case_study/stock/model.json', 'r'))
-
 Stock_price_model = json.loads(open('/app/case_study/stock/model.json', 'r').read())
 
-# sidebar for navigation
-with st.sidebar:
-    
-    selected = option_menu('Stock Price Prediction',
-                          
-                          ['Stock Prediction'],
-                          icons=['activity'],
-                          default_index=0)
-    
-    
-
-
-# page title
-st.title('Stock Price Prediction using ML')
-
 # Define a function to preprocess the text input
-def preprocess_text(text):
+def preprocess_text():
     news = st.text_input('Current News Related to Stock')
-    return current_price
+    return news
 
 # Define the Streamlit app
 def app():
-    
-    
+    # page title
+    st.title('Stock Price Prediction using ML')
+
+    # Text input
+    text_input = preprocess_text()
+
+    # Numeric input
+    current_price = st.number_input('Current Price')
+
     # When the user clicks the 'Predict' button, preprocess the input and pass it to the model
     if st.button('Predict'):
         # Preprocess the text input
-        preprocessed_input = preprocess_text(text_input)
-	
-	#Numeric input
-	current_price = st.number_input('Current Price')
-        
+        preprocessed_input = preprocess_text()
+
         # Convert the preprocessed input to a numpy array
         input_array = np.array([preprocessed_input])
-        
+
         # Use the pre-trained model to make a prediction
         stock_prediction = Stock_price_model.predict(input_array)
-	
-	#prdict final stock value
+
+        # Predict final stock value
         stock_price = current_price + stock_prediction
-        
+
         # Display the prediction to the user
         st.write('Prediction:', stock_price)
-        
+
+# sidebar for navigation
+with st.sidebar:
+    selected = st.selectbox('Stock Price Prediction', ['Stock Prediction'])
+
 # Run the app
 if __name__ == '__main__':
     app()
