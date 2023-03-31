2aa378501da15bdc19f23456bb15fa43fa2c7e59
ash1407 <68329823+ash1407@users.noreply.github.com>
2023-03-31 23:48:03 +0530

    Update Streamlit_stock_prediction.py

--- stock/Streamlit_stock_prediction.py
+++ stock/Streamlit_stock_prediction.py
@@ -27,27 +27,29 @@ st.title('Stock Price Prediction using ML')
 
 # Define a function to preprocess the text input
 def preprocess_text(text):
-    current_price = st.text_input('Current Price')
-    news = st.number_input('Current News Related to Stock')
-    return current_price,news
+    news = st.text_input('Current News Related to Stock')
+    return current_price
 
 # Define the Streamlit app
 def app():
     
-    # Add a text input field for the user to enter their input
-    text_input = st.text_input('Enter your text input:')
     
     # When the user clicks the 'Predict' button, preprocess the input and pass it to the model
     if st.button('Predict'):
         # Preprocess the text input
         preprocessed_input = preprocess_text(text_input)
+	
+	#Numeric input
+	current_price = st.number_input('Current Price')
         
         # Convert the preprocessed input to a numpy array
         input_array = np.array([preprocessed_input])
         
         # Use the pre-trained model to make a prediction
         stock_prediction = Stock_price_model.predict(input_array)
-	stock_price = current_price + stock_prediction
+	
+	#prdict final stock value
+        stock_price = current_price + stock_prediction
         
         # Display the prediction to the user
         st.write('Prediction:', stock_price)
