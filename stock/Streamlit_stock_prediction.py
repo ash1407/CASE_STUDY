75c3d20a3df4b681341e40993d2a8b8b179b16c0
ash1407 <68329823+ash1407@users.noreply.github.com>
2023-03-31 23:54:24 +0530

    Update Streamlit_stock_prediction.py

--- stock/Streamlit_stock_prediction.py
+++ stock/Streamlit_stock_prediction.py
@@ -8,7 +8,7 @@ Stock_price_model = json.loads(open('/app/case_study/stock/model.json', 'r').rea
 
 # Define a function to preprocess the text input
 def preprocess_text():
-    news = st.text_input('Current News Related to Stock')
+    news = st.text_input('Current News Related to Stock', key='text_input')
     return news
 
 # Define the Streamlit app
@@ -20,16 +20,18 @@ def app():
     text_input = preprocess_text()
 
     # Numeric input
-    current_price = st.number_input('Current Price')
+    current_price = st.number_input('Current Price', key='number_input')
 
     # When the user clicks the 'Predict' button, preprocess the input and pass it to the model
-    if st.button('Predict'):
+    if st.button('Predict', key='predict_button'):
         # Preprocess the text input
         preprocessed_input = preprocess_text()
 
-        
+        # Convert the preprocessed input to a numpy array
+        input_array = np.array([preprocessed_input])
+
         # Use the pre-trained model to make a prediction
-        stock_prediction = Stock_price_model.predict(preprocessed_input)
+        stock_prediction = Stock_price_model.predict(input_array)
 
         # Predict final stock value
         stock_price = current_price + stock_prediction
@@ -39,7 +41,7 @@ def app():
 
 # sidebar for navigation
 with st.sidebar:
-    selected = st.selectbox('Stock Price Prediction', ['Stock Prediction'])
+    selected = st.selectbox('Stock Price Prediction', ['Stock Prediction'], key='sidebar')
 
 # Run the app
 if __name__ == '__main__':
