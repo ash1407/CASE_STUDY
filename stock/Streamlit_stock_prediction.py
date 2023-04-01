39318a1bd1afe9db715f08e843cc38be83d9892f
ash1407 <68329823+ash1407@users.noreply.github.com>
2023-04-01 19:26:35 +0530

    Update Streamlit_stock_prediction.py

--- stock/Streamlit_stock_prediction.py
+++ stock/Streamlit_stock_prediction.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 import pandas as pd
+import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, r2_score
 import xgboost as xgb
