b6b9858b37f7ceca543ca837b81fd766d12ed54c
ash1407 <68329823+ash1407@users.noreply.github.com>
2023-04-01 13:54:42 +0530

    Update Streamlit_stock_prediction.py

--- stock/Streamlit_stock_prediction.py
+++ stock/Streamlit_stock_prediction.py
@@ -7,10 +7,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 
 # loading the saved models
-xgb_model  = json.loads(open('/app/case_study/stock/model_tfidf.json', 'r').read())
 
 
-
+xg_reg = xgb.XGBRegressor(objective="reg:squarederror", n_estimators=100, seed=123)
+#xg_reg.load_model("C:\\Users\\qj771f\\Desktop\\Boeing\\Resources\\4_ipynb files\\7_web app\\Streamlit\\stocks\\model_tfidf.json")
+#xgb_model  = json.loads(open('/app/case_study/stock/model_tfidf.json', 'r').read())
+xg_reg.load_model('/app/case_study/stock/model_tfidf.json')
 
 # Load the pre-trained tf-idf vectorizer
 with open('/app/case_study/stock/tfidf.pickle', 'rb') as f:
