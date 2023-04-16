996125b8da5b549b6c18e63d0ccc4a6ebfc63f63
ash1407 <68329823+ash1407@users.noreply.github.com>
2023-04-17 01:04:09 +0530

    Update Streamlit_stock_prediction.py

--- stock/Streamlit_stock_prediction.py
+++ stock/Streamlit_stock_prediction.py
@@ -1,70 +1,69 @@
-import pickle
-import numpy as np
-import pandas as pd
 import streamlit as st
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, r2_score
+import numpy as np
+import pickle
 import xgboost as xgb
-import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from gensim.models import Word2Vec
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # loading the saved models
+xg_reg = xgb.XGBRegressor(objective="reg:squarederror", n_estimators=100, seed=123)
 
-tokenizer = Tokenizer()
-
-
-# Load the tokenizer from a local file
 with open("/app/case_study/stock/tokenizer.pickle", "rb") as f:
     tokenizer = pickle.load(f) 
 
 # Load the model from a local file
 with open("/app/case_study/stock/xg_reg_model.pickle", "rb") as f:
     xg_reg = pickle.load(f)
-    
-model = tf.keras.models.load_model("/app/case_study/stock/embedding_model.h5")
-  
-    
-# Define a function to preprocess the text input
 
+
+with open("/app/case_study/stock/X_tfidf.pickle", "rb") as f:
+    X_tfidf=pickle.load(f)
+    
+
+# Load the pre-trained model
+model = tf.keras.models.load_model("/app/case_study/stock/embedding_model.h5")
+
+# Define a function to preprocess the text input
 def preprocess_text():
     # Create the text input widget with the unique key
     news = st.text_input('Current News Related to Stock')
-
-    input_seq = tokenizer.texts_to_sequences([news])
-
-    # Pad the input sequence to match the maximum sequence length
-    input_seq = pad_sequences(input_seq,maxlen=227)
-
-    # Use the trained Word2Vec model to transform the input sequence
-    input_embedding = model.predict(input_seq)
     
-    return input_embedding
+    # Tokenize and pad the text input
+    sequences = tokenizer.texts_to_sequences([news])
+    X1 = pad_sequences(sequences, maxlen=24)
+    
+    # Transform the text to numeric vectors using Word2Vec
+    embeddings1 = model.predict(X1)
+    
+    # Transform the text to numeric vectors using tf-idf
+    X_tfidf = tfidf.transform([news]).toarray()
+    
+    # Concatenate the two sets of embeddings
+    embeddings = np.concatenate([embeddings1, X_tfidf], axis=1)
+    
+    return embeddings
 
-# Define the Streamlit apps
+# Define the Streamlit app
 def app():
     # page title
     st.title('Stock Price Prediction using ML')
-    
-    # Set the sidebar
-    st.sidebar.title("Stock Prediction App")
-    st.sidebar.write("Enter the current news and price to predict the stock price.")
 
     # Text input
-    text= preprocess_text()
+    text = preprocess_text()
 
     # Numeric input
     current_price = st.number_input('Current Price')
 
     # When the user clicks the 'Predict' button, preprocess the input and pass it to the model
     if st.button('Predict'):
-        # Preprocess the text input
-        
         # Use the pre-trained model to make a prediction
-        stock_prediction = xg_reg.predict(text.reshape(-1, 1))[0]
+        stock_prediction = xgb_model.predict(text.reshape(1,-1))
 
         # Predict final stock value
-        stock_price = current_price + stock_prediction
+        stock_price = current_price + (stock_prediction[0]/100)*current_price 
         
         # Display the prediction to the user
         st.write('Prediction:', stock_price)
